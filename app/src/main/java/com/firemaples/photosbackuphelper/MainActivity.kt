f51b0e0e263d4ba78ee178ee7e4840edf4cad2f4
firemaples <firemaples@gmail.com>
2019-10-10 21:06:03 +0800

    Setup logger

--- app/src/main/java/com/firemaples/photosbackuphelper/MainActivity.kt
+++ app/src/main/java/com/firemaples/photosbackuphelper/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.firemaples.photosbackuphelper
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private val logger by lazy { logger() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        logger.debug("Test")
     }
 }
